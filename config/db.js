fdc19ab844cb601d5a7980cc9f7e61d0fff717d4
MadBoy <73926361+madboy482@users.noreply.github.com>
2025-07-02 15:54:15 +0530

    add

---
+++ config/db.js
@@ -0,0 +1,16 @@
+const mysql = require('mysql2/promise');
+require('dotenv').config();
+
+const db = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 3306,
+  waitForConnections: true,
+  connectionLimit: 5,
+  queueLimit: 0,
+  connectTimeout: 10000
+});
+
+module.exports = db;
